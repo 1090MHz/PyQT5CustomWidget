207179a13842c5e5d9975c12ccb6f74c9b708a0b
1090MHz <107646764+1090MHz@users.noreply.github.com>
2025-02-06 08:26:48 -0600

    Fix translation and rotation

--- custom_vtext.py
+++ custom_vtext.py
@@ -2,35 +2,36 @@ from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 from PyQt5.QtGui import QPainter, QTransform
 from PyQt5.QtCore import Qt, QSize
 
+
 class VerticalLabel(QLabel):
-    def __init__(self, text, parent=None):
-        super(VerticalLabel, self).__init__(text, parent)
+
+    def __init__(self, *args):
+        QLabel.__init__(self, *args)
 
     def paintEvent(self, event):
+        QLabel.paintEvent(self, event)
         painter = QPainter(self)
-        painter.setPen(self.palette().color(self.foregroundRole()))
-        painter.setFont(self.font())
-        
-        # Translate to the bottom-left corner of the widget
-        painter.translate(self.width(), self.height())
-        # Rotate 90 degrees counter-clockwise
+        painter.translate(0, self.height() - 1)
         painter.rotate(-90)
-        
-        # Draw the text
-        painter.drawText(0, 0, self.text())
-        painter.end()
+        self.setGeometry(self.x(), self.y(), self.height(), self.width())
+        QLabel.render(self, painter)
+
+    def minimumSizeHint(self):
+        size = QLabel.minimumSizeHint(self)
+        return QSize(size.height(), size.width())
 
     def sizeHint(self):
-        size = super(VerticalLabel, self).sizeHint()
+        size = QLabel.sizeHint(self)
         return QSize(size.height(), size.width())
+
 
 class VerticalText(QWidget):
     def __init__(self, parent=None):
         super(VerticalText, self).__init__(parent)
-        
+
         self.label = VerticalLabel("Vertical Text", self)
-        
+
         layout = QVBoxLayout(self)
         layout.addWidget(self.label)
-        
+
         self.setLayout(layout)
